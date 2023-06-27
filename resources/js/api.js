d7fa41f8d46ea7d7f451f0631ecd7e1a061a3f45
the creator <chenxi.yan@gmail.com>
2023-06-26 23:51:39 -0400

    update copy.js, api.js, index.css, index.html, about.html, feedback.html, scienc.html

---
+++ resources/js/api.js
@@ -0,0 +1,30 @@
+async function convertText() {
+    const url = 'https://bionic-reading1.p.rapidapi.com/convert';
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': '052e50bde3msh252d405d40faeacp102ddcjsnbbfdf01b05be',
+            'X-RapidAPI-Host': 'bionic-reading1.p.rapidapi.com'
+        },
+        body: new URLSearchParams({
+            content: document.getElementById('inputText').value,
+            response_type: 'html',
+            request_type: 'html',
+            fixation: '1',
+            saccade: '10'
+        })
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        document.getElementById('outputText').innerHTML = result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function toggleNavBar() {
+    const navbarCollapse = document.getElementById('navbarItems');
+    navbarCollapse.classList.toggle('show');
+}
---
+++ resources/js/reset.js
@@ -0,0 +1,4 @@
+function resetText() {
+    document.getElementById("inputText").value = "";
+    document.getElementById("outputText").textContent = "";
+}
